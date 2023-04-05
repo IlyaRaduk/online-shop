8f18f730a133e560723b8c30b3d8abdae4344564
IlyaRaduk <117042097+IlyaRaduk@users.noreply.github.com>
2023-04-05 18:56:05 +0300

    add

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/online-shop">
       <App />
     </BrowserRouter>
   </Provider>
